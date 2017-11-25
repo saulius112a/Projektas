1036e9c2f13fbf898cc3088cd3315f3bd7f5fc89
RacNor <mindaugass66@gmail.com>
2017-11-25 22:58:28 +0200

    initial commit

---
+++ Eshop.Data/DatabaseContext/MySqlDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Eshop.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eshop.Data.DatabaseContext
+{
+    [DbConfigurationType(typeof(MySql.Data.Entity.MySqlEFConfiguration))]
+    public class MySqlDbContext : DbContext
+    {
+        public MySqlDbContext() : base("Default")
+        {
+            //Here we can add some data to db when we initialize db, for now we do not need it
+            //Database.SetInitializer<MySqlDbContext>(new MySqlDbInitializer());
+            Database.SetInitializer<MySqlDbContext>(new MySqlDbInitializer());
+        }
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            
+        }
+    }
+    
+}
---
+++ Eshop.Data/DatabaseContext/MySqlDbInitializer.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+namespace Eshop.Data.DatabaseContext
+{
+    public class MySqlDbInitializer : CreateDatabaseIfNotExists<MySqlDbContext>
+    {
+        protected override void Seed(MySqlDbContext context)
+        {
+            context.Categories.Add(new Entities.Category { Name = "yo" });
+            context.SaveChanges();
+        }
+    }
+}
